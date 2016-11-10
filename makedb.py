bced8f3b63c7c252c4c3b13ad3d883fa088de155
Luan <luan@zoyi.co>
2016-11-10 23:05:18 +0900

    successed sifting a test image

--- makedb.py
+++ makedb.py
@@ -1,17 +1,32 @@
-import sys, os
-# import cv
+import sys	# for commandline arguments
+import os
+import cv2
 
-# img = cv2.imread(sys.argv[1])
+img = cv2.imread(sys.argv[1])
+gray= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+ 
+sift = cv2.xfeatures2d.SIFT_create()
+kp = sift.detect(gray,None)
+ 
+cv2.drawKeypoints(gray,kp,img,flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+cv2.imwrite('sift result',img)
+
 # img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 # sift = cv2.SIFT()
 # keypoints, descriptors = sift.detectAndCompute(img_gray, None)
  
 
-rootDir = os.getcwd() + '/../images/'
-for dirName, subdirList, fileList in os.walk(rootDir):
-    # print('Found directory: %s' % dirName)
-    for fname in fileList:
-        print('\t%s' % dirName + '/' + fname)
+
+
+
+
+
+
+# rootDir = os.getcwd() + '/../images/'
+# for dirName, subdirList, fileList in os.walk(rootDir):
+#     # print('Found directory: %s' % dirName)
+#     for fname in fileList:
+#         print('\t%s' % dirName + '/' + fname)
 
 
 # f = open(os.getcwd() + '/image.jpg', 'r')
