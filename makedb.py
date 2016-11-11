2620fcbc1e30e94d42c959765dcde44774fcbab6
Luan <luan@zoyi.co>
2016-11-12 01:17:39 +0900

    excellent performance of finding original image & downloading elasticsearch ( need to download JDK)

--- makedb.py
+++ makedb.py
@@ -2,27 +2,19 @@ import sys	# for commandline arguments
 import os
 import cv2
 import numpy as np
+from operator import itemgetter
 
-def getKeypoints(imagePath):
-	img = cv2.imread(imagePath)
-	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	sift = cv2.xfeatures2d.SIFT_create()
-	return sift.detect(gray, None)
-
-def makeOutputImage(input, output):
-	img = cv2.imread(input)
-	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	sift = cv2.xfeatures2d.SIFT_create()
-	kp = sift.detect(gray, None)
-	cv2.drawKeypoints(gray,kp,img, flags = cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-	cv2.imwrite(output,img)
-
-def getStringFromKeyPoint(kp):
-	return str(kp.octave)
+featureNum = 50
 
 def getDescriptor(img):
 	sift = cv2.xfeatures2d.SIFT_create()
 	kp, des = sift.detectAndCompute(img, None)
+
+	if (des is None):
+		return np.array([[0.0] * 128, [0.0] * 128], dtype = np.float32)
+	if (len(des) == 1):
+		return np.array(des.tolist()[0] * 2, dtype = np.float32)
+
 	return des
 
 def getMatches(descriptor1, descriptor2):
@@ -32,90 +24,132 @@ def getMatches(descriptor1, descriptor2):
 	flann = cv2.FlannBasedMatcher(index_params,search_params)
 	return flann.knnMatch(descriptor1, descriptor2, k=2)
 
-def getMatchingImage(queryImagePath, trainImagePath):
-	# REFERENCE : http://docs.opencv.org/3.0-beta/doc/py_tutorials/py_feature2d/py_matcher/py_matcher.html
-
-	# Initiate SIFT detector
-	sift = cv2.xfeatures2d.SIFT_create()
-
-	# find the keypoints and descriptors with SIFT
-	kp1, des1 = sift.detectAndCompute(cv2.imread(queryImagePath,0), None)
-	kp2, des2 = sift.detectAndCompute(cv2.imread(trainImagePath,0), None)
-
-	matches = getMatches(des1, des2)
-
-	# Need to draw only good matches, so create a mask
-	matchesMask = [[0,0] for i in range(len(matches))]
-
-	# ratio test as per Lowe's paper
-	for i,(m,n) in enumerate(matches):
-	    if m.distance < 0.7*n.distance:
-	        matchesMask[i]=[1,0]
-
-	draw_params = dict(matchColor = (0,255,0),
-	                   singlePointColor = (255,0,0),
-	                   matchesMask = matchesMask,
-	                   flags = 0)
-
-	return cv2.drawMatchesKnn(img1,kp1,img2,kp2,matches,None,**draw_params)
-
-def drawMatchingImage(queryImagePath, trainImagePath, outputPath):
-	cv2.imwrite(outputPath, getMatchingImage(queryImagePath, trainImagePath))
-
 def encode(descriptor):
 	return descriptor.tostring()
 
 def decode(descriptorString):
-	return np.fromstring(descriptorString, dtype = np.float32)
+	d1arr = np.fromstring(descriptorString, dtype = np.float32)
+	return d1arr.reshape((int(len(d1arr) / 128)), 128)
 
-def lsh(descriptor):
-	# print(type(descriptor[0][0]))
-	print(descriptor[0:2])
-	print(encode(descriptor[0:2]))
-	print(decode(encode(descriptor[0:2])))
-	return descriptor[0:100]
+def compareKeyDescriptor(kp1, kp2):
+	i = 0
+	while i < 128:
+		if kp1[i] > kp2[i]:
+			return True
+		if kp1[i] < kp2[i]:
+			return False
+		i = i + 1
+	return True
+
+def hash(descriptor):
+	des = descriptor.tolist()
+	result = []
+	desLen = len(descriptor)
+	for i in range(featureNum if (featureNum < desLen) else desLen):
+		maxIndex = i
+		j = maxIndex + i + 1
+		while j < desLen:
+			if compareKeyDescriptor(des[j], des[maxIndex]):
+				maxIndex = j
+			j = j + i + 1
+		result.append(des[maxIndex])
+
+	return np.array(result, np.float32)
+
+	return descriptor[0:featureNum]
 
 
-	print(descriptor[::2][0:3])
-	return descriptor[::2]
-	output = []
-	# print([descriptor[0].tolist()])
-	# return descriptor
-	for row in descriptor:
-		output.append(row[0:4].toList())
-	print(output)
-	return output
+
+# def calcSimilarity(matches):
+# 	goodCount = 0
+# 	badCount = 0
+
+# 	for i,(m,n) in enumerate(matches):
+# 	    if m.distance < 0.7 * n.distance:
+# 	    	goodCount = goodCount + 1
+# 	    else:
+# 	    	badCount = badCount + 1
+
+# 	return float(goodCount) / (goodCount + badCount)
+
+def getSimilarity(des1, des2):
+
+	# goodCount = 0
+	# badCount = 0
+
+	point = 0
+
+	# print('start ratio')
+
+	for i,(m,n) in enumerate(getMatches(des1, des2)):
+
+		if m.distance < 0.7 * n.distance:
+			# print('%f %f %f' % (m.distance / n.distance, m.distance, n.distance))
+			point = point + (n.distance / m.distance) ** 3
+			# goodCount = goodCount + 1
+		# else:
+			# badCount = badCount + 1
+
+	# print('end ratio')
+
+	# print('good %d , bad %d' % (goodCount, badCount))
+	# print(float(goodCount) / (goodCount + badCount))
+
+	return point
+
+	# return float(goodCount) / (goodCount + badCount)
+
+def makePair(imagePath):
+	return (imagePath, encode(hash(getDescriptor(cv2.imread(imagePath, 0)))))
+
+def search(queryDescriptor, db):
+	result = []
+	# print(queryDescriptor)
+	# print('it was query')
+	for pair in db:
+		similarity = getSimilarity(queryDescriptor, decode(pair[1]))
+		if similarity > 0:
+			result.append([pair[0], similarity])
+
+	return result
+
+# arg1 = sys.argv[1]
+# arg2 = sys.argv[2]
+# print(hash(getDescriptor(cv2.imread(arg2,0)))[0])
+# print(decode(encode(hash(getDescriptor(cv2.imread(arg2,0)))))[0])
+# print(getSimilarity(hash(getDescriptor(cv2.imread(arg1,0))), decode(encode(hash(getDescriptor(cv2.imread(arg2,0)))))))
+
+# print(encode(hash(getDescriptor(cv2.imread(arg2,0)))))
+
+db = []
+
+rootDir = os.getcwd() + '/../images/4088'
+for dirName, subdirList, fileList in os.walk(rootDir):
+    print('Found directory: %s' % dirName)
+    for fname in fileList:
+    	path = dirName + '/' + fname
+    	extention = os.path.splitext(path)[1]
+    	if (os.path.getsize(path) > 0) and (extention == '.jpg' or extention == '.jpeg' or extention == '.png') :
+    		# print(path)
+    		pair = makePair(path)
+    		if pair:
+    			db.append(pair)
+
+print('start finding')
+
+candidates = sorted(search(hash(getDescriptor(cv2.imread(sys.argv[1], 0))), db), key = itemgetter(1), reverse = True)
+maxScore = candidates[0][1]
+for i in range(1, len(candidates)):
+	if candidates[i][1] < min(1000, maxScore / 4):
+		for answer in candidates[:i]:
+			print('\tsimilarity point : %f , path : %s' % (answer[1], answer[0]))
+		break
 
 
-def getSimilarity(queryImagePath, trainImagePath):
-	des1 = getDescriptor(cv2.imread(queryImagePath,0))
-	des2 = getDescriptor(cv2.imread(trainImagePath,0))
-
-	# print(len(des1))
-	# print(len(des1[0]))
-	# print(len(des2))
-	# print(len(des2[0]))
-
-	matches = getMatches(lsh(des1), lsh(des2))
-
-	goodCount = 0
-	badCount = 0
-
-	for i,(m,n) in enumerate(matches):
-	    if m.distance < 0.7 * n.distance:
-	    	goodCount = goodCount + 1
-	    else:
-	    	badCount = badCount + 1
-
-	return float(goodCount) / (goodCount + badCount)
-
-arg1 = sys.argv[1]
-arg2 = sys.argv[2]
+# for i in range(1, featureNum):
+# 	print(i)
 
 # drawMatchingImage(arg2, arg1, 'output4.jpg')
-print(getSimilarity(arg2, arg1))
-
-
 
 # kp1 = getKeypoints(img1)
 # print(len(kp1))
@@ -169,15 +203,6 @@ print(getSimilarity(arg2, arg1))
 
 
 
-
-
-
-
-# rootDir = os.getcwd() + '/../images/'
-# for dirName, subdirList, fileList in os.walk(rootDir):
-#     # print('Found directory: %s' % dirName)
-#     for fname in fileList:
-#         print('\t%s' % dirName + '/' + fname)
 
 
 # f = open(os.getcwd() + '/image.jpg', 'r')
